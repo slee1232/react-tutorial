b503b59890a0cc30148210ae2d5d204ef9222698
slee1232 <lsmcap91@gmail.com>
2018-08-25 09:46:47 -0400

    conditional output (if statements and ternary operators)

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
+        // const ninjaList = ninjas.map(ninja => {
+        //     if (ninja.age >20){
+        //     return (
+        //         <div className="ninja" key={ninja.id}>
+        //             <div>Name: {ninja.name}</div>
+        //             <div>Age: {ninja.age}</div>
+        //             <div>Belt: {ninja.belt}</div>
+        //     </div>
+        //     )
+        // } else {
+        //     return null
+        // }
+        // })
         const ninjaList = ninjas.map(ninja => {
-            return (
+            return ninja.age >20 ? (
                 <div className="ninja" key={ninja.id}>
                     <div>Name: {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div>
-            </div>
-            )
+                </div>
+            ) : null;
         })
         return(
             <div className="ninja-list">
