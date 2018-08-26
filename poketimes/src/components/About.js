e5b5feb4f122a1fa6945436af4a2c2d17d52df35
slee1232 <lsmcap91@gmail.com>
2018-08-26 12:10:22 -0400

    react router

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4> //typing "lorem" creates the sample text
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quos quidem delectus ducimus deserunt laborum accusamus exercitationem architecto quae voluptas blanditiis sunt veritatis, iste sed perferendis quia totam recusandae! Ab.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4> //typing "lorem" creates the sample text
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quos quidem delectus ducimus deserunt laborum accusamus exercitationem architecto quae voluptas blanditiis sunt veritatis, iste sed perferendis quia totam recusandae! Ab.</p>
+        </div>
+    )
+}
+
+export default Home
