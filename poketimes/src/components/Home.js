1df6ea521d6f4d1e8f71adbf926d69d0e38687dc
slee1232 <lsmcap91@gmail.com>
2018-08-31 14:12:24 -0400

    route parameter (part 2)

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
 state = {
@@ -22,7 +23,9 @@ state = {
                 return (
                     <div className="post card" key={post.id}>
                         <div className="card-content">
+                            <Link to={'/' + post.id}>
                             <span className="card-title">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
